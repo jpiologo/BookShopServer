d964d50b9996c386a0962e1076719b2123d03d0c
João Piologo <joaotpiologo@hotmail.com>
2025-01-28 16:54:38 -0300

    Added more routes and error handling

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,4 +1,4 @@
-const { getTodosLivros , getLivroPorId , insereLivro } = require("../servicos/livro")
+const { getTodosLivros , getLivroPorId , insereLivro , modificaLivro , excluiLivro } = require("../servicos/livro")
 
 function getLivros(req, res) {
 
@@ -16,8 +16,15 @@ function getLivro(req, res) {
 
     try{
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("ID informado não é um formato válido!")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -27,11 +34,59 @@ function getLivro(req, res) {
 
 function postLivro(req, res) {
     try {
+
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send('Livro inserido com sucesso!')
+        if (req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send('Livro inserido com sucesso!')
+        } else {
+            res.status(422)
+            res.send("'Nome' key is required")
+        }
+
     } catch(error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function patchLivro (req, res) {
+    try {
+        const id = req.params.id
+        const body = req.body
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("ID informado não é um formato válido!")
+        }
+
+        modificaLivro(body, id)
+        res.send("Livro modificado com sucesso!")
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
+function deleteLivro(req, res) {
+    try {
+        const id = req.params.id
+
+        if (id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("ID informado não é um formato válido!")
+        }
+
+        excluiLivro(id)
+        res.send("Livro excluído com sucesso!")
+    } catch (error) {
         res.status(500)
         res.send(error.message)
     }
@@ -40,7 +95,9 @@ function postLivro(req, res) {
 module.exports = {
     getLivros,
     getLivro,
-    postLivro
+    postLivro,
+    patchLivro,
+    deleteLivro
 }
 
 //
