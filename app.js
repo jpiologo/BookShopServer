950ff6b6c6f89af21e761a503734e7efeeb74735
Joao Piologo <joaotpiologo@hotmail.com>
2025-01-29 22:17:52 -0300

    Added Cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const port = 8000
 const rotaLivro = require('./rotas/livro')
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors({origin: "*"}))
 app.use('/livros', rotaLivro)
 
 app.listen(port, () => {
@@ -13,3 +15,7 @@ app.listen(port, () => {
 //Para saber mais sobre o Express JS: https://expressjs.com/pt-br/
 //Para dar start no servidor: nodemon app.js
 //Para saber mais sobre códigos de respostas http: https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status
+
+//nodemon app.js
+//npm install express
+//npm install cors
